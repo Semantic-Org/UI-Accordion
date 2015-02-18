98072d112da9353b968902f3353a33d45007baa8
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:24:25 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-accordion',
+  summary : 'Semantic UI - Accordion (official): Single component release of accordion',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Accordion.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
