cde476afa8f55a9cba9902fa6e5bedee09383a3d
Pusher Robot <jack@semantic-ui.com>
2015-04-13 15:22:29 -0400

    Updated component to version 1.11.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-accordion',
   summary : 'Semantic UI - Accordion: Single component release',
-  version : '1.11.6',
+  version : '1.11.7',
   git     : 'git://github.com/Semantic-Org/UI-Accordion.git',
 });
 
