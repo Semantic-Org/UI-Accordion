27be6a202e610a9860413ed6a33e317e36f55a6a
Jack Lukic <jacklukic@gmail.com>
2017-03-29 06:35:32 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-accordion',
   summary : 'Semantic UI - Accordion: Single component release',
-  version : '2.2.9',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Accordion.git',
 });
 
